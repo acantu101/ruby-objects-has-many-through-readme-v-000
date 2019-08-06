20dfde2577f16cd384f61c5195f98a33b5b65f72
alejandra cantu <alejandra.cantu13@gmail.com>
2019-08-06 02:07:01 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -14,8 +14,8 @@ class Customer
     @@all
   end
 
-  def new_meal(customer, total, tip = 0)
-   Meal.new(self, waiter, total, tip)
+  def new_meal(self, total, tip = 0)
+   Meal.new(waiter, self, total, tip)
  end
 
  def meals
